f88c00862fe0f999c80dd2585388415db8b6e555
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-25 15:12:22 +0200

    add completed ms-grid

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 //DOM ref
 const msGrid = document.querySelector('.ms-grid');
 
-
-//  create a cycle with for (from 1 to 100) {
-//  -create li elements (add css classes)
-//  -append in ul
-//  }
-
+//ms-grid
 for (let i = 1; i <= 100; i++) {
     const li = document.createElement('li');
     li.className = 'square';
     li.classList.add('d-flex', 'justify-content-center', 'align-items-center');
-    li.append(`${i}`)
+
+    if (i % 3 == 0 && i % 5 !== 0) {
+        li.classList.add('bg-acquamarine');
+        li.append(`fizz`);
+    }
+
+    else if (i % 5 == 0 &&  i % 3 !== 0 ) {
+        li.classList.add('bg-ocra');
+        li.append(`buzz`);
+    }
+
+    else if (i % 3 == 0 && i % 5 == 0) {
+        li.classList.add('bg-red');
+        li.append(`fizzbuzz`);
+    }
+
+    else {
+        li.classList.add('bg-blue');
+        li.append(`${i}`);
+    }
+
     msGrid.append(li);
 }
+
+
 
