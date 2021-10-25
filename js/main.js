456977e7efcd789af94f6570dd3056dbe53b855e
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-25 14:40:47 +0200

    add sketch-grid

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 //DOM ref
+const msGrid = document.querySelector('.ms-grid');
+
 
 //  create a cycle with for (from 1 to 100) {
 //  -create li elements (add css classes)
 //  -append in ul
 //  }
+
+for (let i = 1; i <= 100; i++) {
+    const li = document.createElement('li');
+    li.className = 'square';
+    li.classList.add('d-flex', 'justify-content-center', 'align-items-center');
+    li.append(`${i}`)
+    msGrid.append(li);
+}
 
